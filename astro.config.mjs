ff77d52dcdbc88e45346349277b0b05b25d22e89
Arpan Chattopadhyay <arpanchattopadhyay23@gmail.com>
2023-12-01 20:04:54 +0530

    add env variable

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
+import { loadEnv } from "vite";
+import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
-import tailwind from "@astrojs/tailwind";
+const { SITE_DOMAIN } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
 
 // https://astro.build/config
 export default defineConfig({
+  site: SITE_DOMAIN,
   integrations: [tailwind()],
 });
